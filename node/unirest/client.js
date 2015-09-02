934167fb92bbdfc4d946e9fc1a8101c6790547cc
Bryan Barnard <bryan.barnard@servicenow.com>
2015-09-02 09:15:16 -0500

    baseline

--- node/unirest/client.js
+++ node/unirest/client.js
@@ -1,3 +1,13 @@
+/**
+ * @description Sample Request Against ServiceNow REST Table API
+ * Gets a single configuration item from DemoNightlytUS ServiceNow Sandbox Instance
+ * Returning Configuration Item as JSON
+ *
+ * Uses Basic Authentication to make the request and the the Mashape Unirest Node.js library
+ */
+
+
+
 var unirest = require('unirest');
 var Request = unirest.get('https://demonightlyus.service-now.com/api/now/table/cmdb_ci?sysparm_limit=1');
 
