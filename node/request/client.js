4b0f74e38b5687a49c9ebb7f17aa5613c1e8a79c
Bryan Barnard <bryan.barnard@servicenow.com>
2015-09-26 23:11:50 -0500

    cleanup

--- node/request/client.js
+++ node/request/client.js
@@ -10,7 +10,8 @@
 
     var options = {
         uri: 'https://demonightlyus.service-now.com/api/now/table/cmdb_ci?sysparm_limit=1',
-        time:true,
+        method: 'GET',
+        time:true, //causes each request to be timed
         auth: {
             'user': 'admin',
             'pass': 'snow2004',
@@ -23,18 +24,12 @@
     };
 
 
-    request( options,function (error, response, body) {
-
+    var callback = function (error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log('elapsed_time: ' + response.elapsedTime);
         }
+    };
 
-
-        request(options ,function (error, response, body) {
-
-            if (!error && response.statusCode == 200) {
-                console.log('elapsed_time: ' + response.elapsedTime);
-            }
-        });
-    });
+    //make the request, note that subsequent requests will send cookies if cookies were received in first response
+    request(options, callback);
 })();
